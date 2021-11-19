7e9f5327a8f4cc7ad5d9ce26bdbaad510fb480b1
kenchaso <kenchasonakai@gmail.com>
2021-11-19 11:33:07 +0900

    finish

--- hack.rb
+++ hack.rb
@@ -1,34 +1,45 @@
-require 'selenium-webdriver'
-require 'capybara'
-require 'capybara/dsl'
-include Capybara::DSL
+class Tabelog
+  require 'selenium-webdriver'
+  require 'pry-byebug'
+  require "csv"
+  require 'nokogiri'
+  require 'capybara'
+  require 'capybara/dsl'
+  include Capybara::DSL
+  load './capybara_config.rb'
+  load './capybara_register.rb'
 
-# https://www.rubydoc.info/gems/capybara/Capybara.configure
-Capybara.configure do |config|
-  config.default_driver = :chrome
-  config.javascript_driver = :chrome
-  # Rackアプリのサーバーを起動するかの設定
-  config.run_server = false
-  config.default_selector = :css
-  config.default_max_wait_time = 5
-  config.ignore_hidden_elements = true
-  config.save_path = Dir.pwd
-  config.automatic_label_click = false
+  def find_store(area:, food:)
+    shop_name, place, ratio, holiday, shop_list = [".list-rst__rst-name", ".list-rst__area-genre", ".c-rating__val", ".list-rst__holiday-text", ".list-rst__wrap"]
+    visit "https://tabelog.com"
+    find("input[id='sa']").set(area)
+    find("input[id='sk']").set(food)
+    find("#js-global-search-btn").click
+    target_html_array = page.all(shop_list).map{ |element| Nokogiri::HTML.parse(element['innerHTML'], nil, 'utf-8') }
+    shop_info_array = target_html_array.map { |html| {
+      shop_name: get_info(html, shop_name),
+      place: get_info(html, place),
+      ratio: get_info(html, ratio),
+      holiday: get_info(html, holiday),
+      url: get_info(html, "url") }
+    }
+
+    write_csv(shop_info_array)
+  end
+
+  private
+
+  def get_info(doc, target)
+    return doc.css("a")[0][:href] if target == "url"
+    doc.css(target).text.strip
+  end
+
+  def write_csv(hash_array)
+    CSV.open("./shop_info.csv", "wb") do |csv|
+      csv << hash_array[0].keys
+      hash_array.each{ |hash| csv << hash.values }
+    end
+  end
 end
 
-
-Capybara.register_driver :chrome do |app|
-
-  # ブラウザーを起動する
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-  )
-end
-
-visit "https://tabelog.com/tokyo/A1303/A130301/"
-# require 'debug'
-select "￥2,000", from: "lstcost-sidebar"
-find("input[name='sk']").set("焼肉")
-# fill_in "input[name='sk']", with: "aaa"
-find("button[aria-label='検索']").click
+#Tabelog.new.find_store(area: "志木", food: "焼肉")
